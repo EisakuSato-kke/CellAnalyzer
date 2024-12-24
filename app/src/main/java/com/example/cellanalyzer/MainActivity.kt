0b68e8e0c94ac2af657d875e47f95955e3808ba8
EisakuSato-kke <eisaku-sato@kke.co.jp>
2024-12-24 13:13:07 +0900

    表示する情報を追加

--- app/src/main/java/com/example/cellanalyzer/MainActivity.kt
+++ app/src/main/java/com/example/cellanalyzer/MainActivity.kt
@@ -58,49 +58,55 @@ class MainActivity : AppCompatActivity() {
             if (!cellInfoList.isNullOrEmpty()) {
                 val info = StringBuilder()
 
-                for (cellInfo in cellInfoList) {
-                    when (cellInfo) {
-                        is CellInfoLte -> {
-                            val cellSignalStrengthLte = cellInfo.cellSignalStrength
-                            val cellIdentityLte = cellInfo.cellIdentity
+                for (cellInfo in cellInfoList) when (cellInfo) {
+                    is CellInfoLte -> {
+                        val cellSignalStrengthLte = cellInfo.cellSignalStrength
+                        val cellIdentityLte = cellInfo.cellIdentity
 
-                            if (cellInfo.isRegistered) {
-                                info.append("Connected LTE Cell:\n")
-                            } else {
-                                info.append("Nearby LTE Cell:\n")
-                            }
-
-                            val rssi = cellSignalStrengthLte.dbm
-                            val rsrp = cellSignalStrengthLte.rsrp
-                            val rsrq = cellSignalStrengthLte.rsrq
-                            val band = cellIdentityLte.earfcn
-
-                            info.append("LTE: RSSI: $rssi dBm\n")
-                            info.append("LTE: RSRP: $rsrp dBm\n")
-                            info.append("LTE: RSRQ: $rsrq dB\n")
-                            info.append("LTE: EARFCN: $band\n\n")
+                        if (cellInfo.isRegistered) {
+                            info.append("Connected LTE Cell:\n")
+                        } else {
+                            info.append("Nearby LTE Cell:\n")
                         }
-                        is CellInfoNr -> {
-                            val cellSignalStrengthNr = cellInfo.cellSignalStrength
-                            val signalStrengthNr = cellInfo.cellSignalStrength as CellSignalStrengthNr
-                            val cellIdentityNr = cellInfo.cellIdentity as CellIdentityNr
 
-                            if (cellInfo.isRegistered) {
-                                info.append("Connected 5G NR Cell:\n")
-                            } else {
-                                info.append("Nearby 5G NR Cell:\n")
-                            }
+                        val rssi = cellSignalStrengthLte.dbm
+                        val rsrp = cellSignalStrengthLte.rsrp
+                        val rsrq = cellSignalStrengthLte.rsrq
+                        val earfcn = cellIdentityLte.earfcn
+                        val band = cellIdentityLte.bands
+                        val bandwidth = cellIdentityLte.bandwidth
+                        val NWoperator = cellIdentityLte.mobileNetworkOperator
 
-                            val ssRsrp = signalStrengthNr.ssRsrp
-                            val ssRsrq = signalStrengthNr.ssRsrq
-                            val ssSinr = signalStrengthNr.ssSinr
-                            val nrarfcn = cellIdentityNr.nrarfcn
+                        info.append("RSSI: $rssi dBm\n")
+                        info.append("RSRP: $rsrp dBm\n")
+                        info.append("RSRQ: $rsrq dB\n")
+                        info.append("EARFCN: $earfcn\n")
+                        info.append("BAND: $band\n")
+                        info.append("BANDWIDTH: $bandwidth\n")
+                        info.append("NetworkOperator: $NWoperator\n\n")
+                    }
+                    is CellInfoNr -> {
+                        val cellSignalStrengthNr = cellInfo.cellSignalStrength
+                        val signalStrengthNr = cellInfo.cellSignalStrength as CellSignalStrengthNr
+                        val cellIdentityNr = cellInfo.cellIdentity as CellIdentityNr
 
-                            info.append("NR: RSRP: $ssRsrp dBm\n")
-                            info.append("NR: RSRQ: $ssRsrq dB\n")
-                            info.append("NR: SINR: $ssSinr dB\n")
-                            info.append("NR: ARFCN: $nrarfcn\n\n")
+                        if (cellInfo.isRegistered) {
+                            info.append("Connected 5G NR Cell:\n")
+                        } else {
+                            info.append("Nearby 5G NR Cell:\n")
                         }
+
+                        val ssRsrp = signalStrengthNr.ssRsrp
+                        val ssRsrq = signalStrengthNr.ssRsrq
+                        val ssSinr = signalStrengthNr.ssSinr
+                        val nrarfcn = cellIdentityNr.nrarfcn
+                        val bands = cellIdentityNr.bands
+
+                        info.append("RSRP: $ssRsrp dBm\n")
+                        info.append("RSRQ: $ssRsrq dB\n")
+                        info.append("SINR: $ssSinr dB\n")
+                        info.append("ARFCN: $nrarfcn\n")
+                        info.append("BAND: $bands\n\n")
                     }
                 }
 
